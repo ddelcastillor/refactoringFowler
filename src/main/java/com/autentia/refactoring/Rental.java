6cb0d2b81f26cfcc197b10da6049cb89f84d92e9
David del Castillo <ddelcastillor.david@gmail.com>
2018-08-04 14:17:30 +0200

    - Mover logica amountFor a Rental.getCharge - Usar aRental.getCharge en Customer.amountFor

--- src/main/java/com/autentia/refactoring/Rental.java
+++ src/main/java/com/autentia/refactoring/Rental.java
@@ -14,4 +14,24 @@ public class Rental {
     public Movie getMovie() {
       return movie;
     }
+    
+	public double getCharge(Rental aRental) {
+		double thisAmount=0;
+		switch (aRental.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (aRental.getDaysRented() > 2)
+				thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += aRental.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (aRental.getDaysRented() > 3)
+				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
 }
